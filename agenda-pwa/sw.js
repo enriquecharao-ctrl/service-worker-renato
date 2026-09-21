156f58694ef0dc9ba4bdc02af1a1b3def0f886c3
enriquecharao-ctrl <enrique.charao@estudante.ifms.edu.br>
2026-09-21 15:27:05 -0400

    atividade em sala

--- agenda-pwa/sw.js
+++ agenda-pwa/sw.js
@@ -1,12 +1,20 @@
+const CACHE_NAME = "agenda-v1"
+const ARQUIVOS = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./icon-192x192.png"
+];
+
 self.addEventListener("install", event => {
-    console.log("Service Worker instalado");
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+    );
 });
 
 self.addEventListener("activate", event => {
     console.log("Service Worker ativado");
-});
-self.addEventListener("fetch", event => {
-    console.log("Requisição:", event.request.url)
 });
 self.addEventListener("fetch", (event) => {
     if (event.request.url.endsWith("/teste-sw")) {
